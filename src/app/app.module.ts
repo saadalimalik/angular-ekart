20afef8a916ddddeb8283e87b482549d3423527e
saadalimalik <saad.malik6366@gmail.com>
2024-04-27 16:09:45 +0500

    Structuring the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
 import { NgModule } from '@angular/core';
 import {
-  BrowserModule,
-  provideClientHydration,
+    BrowserModule,
+    provideClientHydration,
 } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { TopHeaderComponent } from './header/top-header/top-header.component';
-import { TopNavComponent } from './header/top-nav/top-nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TopHeaderComponent } from './components/header/top-header/top-header.component';
+import { TopNavComponent } from './components/header/top-nav/top-nav.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { MainNavComponent } from './header/main-nav/main-nav.component';
+import { MainNavComponent } from './components/header/main-nav/main-nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    TopHeaderComponent,
-    TopNavComponent,
-    MainNavComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
-  providers: [provideClientHydration()],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        TopHeaderComponent,
+        TopNavComponent,
+        MainNavComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+    providers: [provideClientHydration()],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
