e771238b976aa3612f1be37accfa2e9e30a428e1
Saad Ali Malik <saad.malik6366@gmail.com>
2024-05-06 09:12:57 +0500

    chore: Update header component to include main navigation links

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { NavItem } from '../../../interfaces/general.interfaces';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    navItems: NavItem[] = [
+        {
+            text: 'Home',
+            link: '/',
+        },
+        {
+            text: 'Products',
+            link: '/products',
+        },
+        {
+            text: 'Contact Us',
+            link: '/contact',
+        },
+        {
+            text: 'About Us',
+            link: '/about',
+        },
+    ];
+}
--- src/app/components/header/main-nav/main-nav.component.ts
+++ src/app/components/header/main-nav/main-nav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NavItem } from '../../../../interfaces/general.interfaces';
 
 @Component({
     selector: 'main-nav',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class MainNavComponent {
     searchValue: string = '';
+    @Input() navItems: NavItem[] = [];
 }
